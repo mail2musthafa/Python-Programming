12b9f91144d74011a510ee8b9101627e0d275fbe
mail2musthafa <mail2musthafa.md@gmail.com>
2024-02-07 11:07:16 +0530

    introduction to python
    Basic under standing about python

---
+++ day1.py
@@ -0,0 +1,11 @@
+print("welcome to the Python Programming!😍")
+# Introduction to python 
+# Pdthon is a widely used progrmming  general purpose programming language 
+# this entire refernce code is took from greek forgeek all credits goed to geek for geeks
+
+# writing first  first program ?
+print("this is our first prunt function !lets strt writing some mpre ")
+
+# is hash pthon interpreter  ignore this if start with #
+#print is work to print somthing usnig built in function 
+#called print(" in qouts ")
