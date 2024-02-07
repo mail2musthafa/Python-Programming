2cafd0b4cd0eca72273f515eaa1d0d21ad7a2d39
mail2musthafa <mail2musthafa.md@gmail.com>
2024-02-07 11:59:48 +0530

    day1.py
    python day1

--- day1.py
+++ day1.py
@@ -16,5 +16,39 @@ def Main():
 # for 'Main' function existence 
 if __name__=="__main__": 
     Main() 
-  #------------------------------  
-     
+  #------------------------------ 
+    # for iteratig the (looping the) 
+for i in range(10):
+    print(i)
+#-------------------------------------------
+#  Pthon program to illustrate math module 
+import math
+def Main():
+    num =-49 
+    num = math.fabs(num)  
+    print(num)     
+if __name__ =="__main__":
+    Main()       
+#---------------------------------------------------
+# Python Taking the inputs form user  
+# input(prompt)
+# raw_input(prompt)  
+var = input("please enter value")
+print(var)   
+name = input("please enter your name!!?\n") 
+print(name)        
+
+ #--------------------------------------------------
+name = input('What is your name?\n')     # \n ---> newline  ---> It causes a line break 
+print(name)    
+#-----------------------------------
+#program to check input type in python 
+num = input("please enter the value ")
+print(num)
+num1 = input('please enter value')
+print(num1)
+print("type of number is :",type(num1))
+#--------------------------------------------------
+g = raw_input("Enter your name : ") 
+print g    
+#--------------------------------
