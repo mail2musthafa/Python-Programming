00b713f8f0a0b01dff5ed10f88e1222d82c23742
mail2musthafa <mail2musthafa.md@gmail.com>
2024-02-13 09:40:39 +0530

    commit
    updated

--- day1.py
+++ day1.py
@@ -63,14 +63,4 @@ num1 = int(input("please enter value \n"))
 num1 = float(input("enter the value \n"))
 num1 = str(input("please enter the string value"))
 #----------------------------------
- 
-#taqking the mmultiple inputs  isimng ṭh
-# e using split method 
-#accept multi[ler multple inputys usingh the usomng the
-#]
-#sploit functions x,y
-
-
-
-print(type(num1))
-
+print("welcome!")
